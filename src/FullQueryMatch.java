dfedf74768e1e08d66a386978cfecbbbcd26ea67
theWhyteRabbit <svidal@ualberta.ca>
2015-11-21 21:21:28 -0700

    working on full match queries

---
+++ src/FullQueryMatch.java
@@ -0,0 +1,29 @@
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import com.sleepycat.db.*;
+
+Public class FullMatchQuery{
+	
+	/* futue implementation will involve this class
+	 * accessing databases which have already been created
+	 * (ie. not creating a new database for each query */
+	public FullMatchQuery(){
+		
+	}
+	
+	/* "p:" search using pt.idx*/
+	public void ptermsQuery(String key){
+		
+	}
+	
+	/* "r:" search using rt.idx*/
+	public void rtermsQuery(String key){
+		
+	}
+	
+	/* search both pt.idx and rt.idx*/
+	public void ptermsAndRtermsQuery(String key){
+		
+	}
+}
